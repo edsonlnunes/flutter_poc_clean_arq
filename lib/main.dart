d85a7be6940ebb0830834a8f4cd131b32b7bf82e
Edson Martins <edsonmartins3110@gmail.com>
2021-02-13 05:07:45 -0300

    adicionado dartz

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'counter/controller_change_notifier.dart';
-import 'counter/counter.page.dart';
+import 'package:dio/dio.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,7 +14,120 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: CounterPage(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String valor = 'Quinta';
+  @override
+  Widget build(BuildContext context) {
+    var content = <Widget>[
+      RichText(
+          text: TextSpan(text: 'Não tem cadastro? ', children: [
+        TextSpan(
+            text: 'Clique aqui para fazer o cadastro.',
+            style: TextStyle(
+              decoration: TextDecoration.underline,
+            ))
+      ])),
+    ];
+
+    var actions = <Widget>[
+      RaisedButton(
+        child: Text('Fechar'),
+        onPressed: () {},
+      ),
+      RaisedButton(
+        child: Text('Continuar'),
+        onPressed: () {},
+        color: Colors.blue,
+      ),
+    ];
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Teste'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 15,
+          ),
+          child: Center(
+            child: Container(
+              width: double.infinity,
+              child: RaisedButton(
+                color: Colors.blue,
+                child: Text('Mostrar modal'),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (ctx) {
+                      return AlertDialog(
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: InkWell(
+                                child: Icon(
+                                  Icons.close,
+                                  color: Color(0xffe16e0e),
+                                  size: 30,
+                                ),
+                                onTap: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ),
+                            Text(
+                              'Titulo qualquer',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline6
+                                  .copyWith(
+                                    color: Colors.orange,
+                                  ),
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            ...content,
+                            SizedBox(
+                              height: 15,
+                            ),
+                            resolveAcoes(actions),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ),
+        ));
+  }
+
+  Widget resolveAcoes(List<Widget> actions) {
+    if (actions.length == 1) {
+      return Align(
+        alignment: Alignment.center,
+        child: Container(
+          width: double.infinity,
+          child: actions[0],
+        ),
+      );
+    }
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: actions,
     );
   }
 }
